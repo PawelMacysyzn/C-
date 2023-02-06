7515fe42076dbab44994d063fe32e3628fc2d57c
Pawel Macyszyn <macyszyn78@gmail.com>
2023-02-06 22:58:52 +0100

    Next orgin C++

---
+++ hints.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+
+    double a = 2.2;
+
+    // check variable type || sprawdzanie typu zmiennej
+    // cout << typeid((float)a).name() << endl;
+
+    return 0;
+}
