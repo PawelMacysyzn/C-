00ea6895e29c34df5718393d58ab2834676d15b9
Pawel Macyszyn <macyszyn78@gmail.com>
2023-02-09 12:47:04 +0100

    Day of week in polish

---
+++ Orgin/day_of_week.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+int main()
+{
+    string dayOfWeek;
+    cout << "Podaj dzien tygodnia" << endl;
+    cin >> dayOfWeek;
+
+    for (int i = 0; i < dayOfWeek.length(); i++)
+        dayOfWeek[i] = toupper(dayOfWeek[i]);
+    
+
+    if (dayOfWeek == "PONIEDZIALEK" || dayOfWeek == "WTOREK" || dayOfWeek == "SRODA")
+        cout << "Jescze kilka dni" << endl;
+    else if (dayOfWeek == "CZWARTEK")
+        cout << "Maly piątek" << endl;
+    else if (dayOfWeek == "PIATEK")
+        cout << "Juz za moment" << endl;
+    else if (dayOfWeek == "SOBOTA" || dayOfWeek == "NIEDZIELA")
+        cout << "Weekend!" << endl;
+    else
+        cout << "Error.." << endl;
+
+    return 0;
+}
