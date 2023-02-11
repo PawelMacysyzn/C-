ca8699689c11d8fc649151b171bbd0f5255c986d
Pawel Macyszyn <macyszyn78@gmail.com>
2023-02-11 14:32:15 +0100

    Exanole of set

---
+++ Orgin/set.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    set<string> names = {"Pawel", "Mariusz", "Kamil", "Dominik", "Pawel", "Adam"};
+
+    for (string name : names)
+        cout << name << " ";
+    cout << endl;
+
+    // Adam Dominik Kamil Mariusz Pawel
+
+    
+    names.insert("Harry");
+
+    for (string name : names)
+        cout << name << " ";
+    cout << endl;
+
+    // Adam Dominik Harry Kamil Mariusz Pawel 
+
+
+    /*names.clear();
+
+    for (string name : names)
+        cout << name << " ";
+    cout << endl;
+
+    // none */
+
+
+    names.erase("Pawel");
+
+    for (string name : names)
+        cout << name << " ";
+    cout << endl;
+
+    // Adam Dominik Harry Kamil Mariusz
+
+}
