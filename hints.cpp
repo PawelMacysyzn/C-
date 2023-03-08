9c817b69f16f95a6ba14956293139943e571c73c
Pawel Macyszyn <macyszyn78@gmail.com>
2023-03-08 18:03:34 +0100

    More hints

--- hints.cpp
+++ hints.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int main(void)
+/*int main(void)
 {
 
     double a = 2.2;
@@ -10,6 +10,69 @@ int main(void)
     // EN: check variable type PL: sprawdzanie typu zmiennej
     cout << typeid((float)a).name() << endl;
     // f
+    return 0;
+}*/
+
+/***********************************************************/
+
+/*int var = 10;
+
+int main(void)
+{
+
+    int var = 20;
+
+
+    cout << var << endl;
+    // 20
+    // Dostep do zmiennej globalnej
+    cout << ::var << endl;
+    // 10
 
     return 0;
-}
+}*/
+
+/***********************************************************/
+
+/*int main(void)
+{
+
+    signed int int1 = 20;
+    // Rozmiar zmiennej w bait -ach
+    cout << sizeof(int1) << endl;
+    // 4
+
+    long long int var1 = 14;
+    cout << sizeof(var1) << endl;
+    // 8
+
+    long int var2 = 14;
+    cout << sizeof(var1) << endl;
+    // 8
+
+    unsigned char char1 = 'z';
+    cout << sizeof(char1) << endl;
+    // 1
+
+    long double double1;
+    cout << sizeof(double1) << endl;
+    // 16
+
+    return 0;
+}*/
+
+/***********************************************************/
+/*
+#define CONST 50
+
+int main()
+{
+
+    cout << sizeof(CONST) << endl;
+    // 4
+    cout << CONST << endl;
+    // 50
+    return 0;
+}*/
+
+/***********************************************************/
