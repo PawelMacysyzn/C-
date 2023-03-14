cc04a3d1af8a41c7033a81a2f61de0a9015b3f14
Pawel Macyszyn <macyszyn78@gmail.com>
2023-03-14 18:17:49 +0100

    Small changes

---
+++ CPP COURSE/Operators/introduction.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+// W C++ wyróżnia się 5 operatorów,        There are 5 operators in C++
+
+// +   PL: Operator dodawania,             Eng: Addition operator
+// -   PL: Operator odejmowania            Eng: Subtraction operator
+// *   PL: Operator mnożania               Eng: Multiplication operator
+// /   PL: Operator dzielenia              Eng: Division operator
+// %   PL: Operator modulo                 Eng: Operator modulo
+
+int main()
+{
+    float f1 = 10, f2 = 5, result;
+
+    return 0;
+}
