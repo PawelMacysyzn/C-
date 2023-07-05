c6160138ab7f284dbc71d12a92edeb8a15efa93a
Pawel Macyszyn <macyszyn78@gmail.com>
2023-07-05 16:09:22 +0200

    Divide project for small pieces

--- Mentoring_CPP/main.cpp
+++ Mentoring_CPP/main.cpp
@@ -1,19 +1,8 @@
 #include <iostream>
+#include "args_operations.h"
 
-int main (int argc, char ** argv) {
-    std::cout << "Program options:" << std::endl;
-
-    // 1. argv[0]
-    // 2. argv[1]
-    // ...
-
-    // for (auto& arg : (argv))
-    int tab[]= {1,2,3,4};
-    // for (int i : tab) {
-    for (auto it = std::begin(tab); it != std::end(tab); ++it) {
-        std::cout << *it << ",";
-    }
-    std::cout << std::endl;
-
+int main(int argc, char **argv)
+{
+    show_args(argc, argv);
     return 0;
 }
