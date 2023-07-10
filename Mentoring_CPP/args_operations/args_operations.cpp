5b1c44650f4f9a4f237c47b8a99cf27d578a0928
Pawel Macyszyn <macyszyn78@gmail.com>
2023-07-10 20:40:20 +0200

    Added function add_args_to_vector

--- Mentoring_CPP/args_operations/args_operations.cpp
+++ Mentoring_CPP/args_operations/args_operations.cpp
@@ -1,7 +1,15 @@
 #include "args_operations.h"
-#include <iostream>
 
-void show_args(int argc, char **argv)
+void add_args_to_vector(std::vector<std::string> &arg_of_main, int argc, char **argv)
+{
+    for (int i = 0; i < argc; i++)
+    {
+        // std::cout << "argv[" << i << "] = " << argv[i] << std::endl;
+        arg_of_main.push_back(argv[i]);
+    }
+}
+
+void show_args(std::vector<std::string> arg_of_main)
 {
     std::cout << "Program options:" << std::endl;
 
@@ -9,12 +17,13 @@ void show_args(int argc, char **argv)
     // 2. argv[1]
     // ...
 
-    // for (auto& arg : (argv))
-    int tab[] = {1, 2, 3, 4};
-    // for (int i : tab) {
-    for (auto it = std::begin(tab); it != std::end(tab); ++it)
+    //
+    int num = 1;
+    for (const std::string &elem : arg_of_main)
     {
-        std::cout << *it << ",";
+        std::cout << "argv[" << num << "] = " << elem << std::endl;
+        num++;
     }
+
     std::cout << std::endl;
 }
--- Mentoring_CPP/args_operations/args_operations.h
+++ Mentoring_CPP/args_operations/args_operations.h
@@ -1,3 +1,7 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include <string>
 
-void show_args(int argc, char **argv);
+void add_args_to_vector(std::vector<std::string> &arg_of_main, int argc, char **argv);
+void show_args(std::vector<std::string> arg_of_main);
