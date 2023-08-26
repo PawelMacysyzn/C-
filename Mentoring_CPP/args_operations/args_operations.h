7059cc139b5f3fce9a6a33c00f0acb42dd50656d
Pawel Macyszyn <pawel.macyszyn@yaramarine.com>
2023-08-26 13:20:35 +0200

    Cleanup in the repo

--- Mentoring_CPP/args_operations/args_operations.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <iostream>
-#include <vector>
-#include <string>
-
-void add_args_to_vector(std::vector<std::string> &arg_of_main, int argc, char **argv);
-void show_args(std::vector<std::string> arg_of_main);
